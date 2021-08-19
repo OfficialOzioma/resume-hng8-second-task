ade34efee6cb963c52cf69927879cad4f1df5656
Ozioma Dike <pascalozioma18@gmail.com>
2021-08-19 12:18:09 +0100

    my second task in HNG 8 intenship

---
+++ contact.php
@@ -0,0 +1,44 @@
+<?php
+ob_start();
+// database connection code
+if(isset($_POST['submit']))
+{
+	// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
+	$con = mysqli_connect('localhost', 'root', 'root','Bus_dir');
+	// $con = mysqli_connect('db4free.net', 'laravel_link', 'laravel_link','laravel_link');
+
+	// get the post records
+
+	$txtName = $_POST['name'];
+	$txtEmail = $_POST['email'];
+	$txtPhone = $_POST['phone'];
+	$txtMessage = $_POST['message'];
+
+	// database insert SQL code
+	// $sql = "INSERT INTO `laravel_link` (`Id`, `name`, `email`, `phone`, `message`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+	$sql = "INSERT INTO `contact` (`Id`, `name`, `email`, `phone`, `message`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone', '$txtMessage')";
+
+	// insert in database 
+	$rs = mysqli_query($con, $sql);
+	if($rs)
+	{
+
+		echo "<script type='text/javascript'>
+				alert('Thank you {$txtName} for contacting me, I will get back to you shortly');
+				window.location.href='resume.php#contact'; 
+			</script>";
+		
+
+	}
+
+}
+else
+{
+	echo "<script type='text/javascript'>
+				alert('Sorry! the message wasn`t sent');
+				window.location.href='resume.php#contact'; 
+			</script>";
+	
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+	echo 'Hello Ozioma Dike';
+	echo "<br />";
+	echo "<br />";
+	echo "Click <a href='resume.php' target='blank'> Here</a> to view my resume";
+ ?>
